7e1ec1fa56a14d87a8591f18a47e624c262c1080
z4trox <kylian.lefebvre60@gmail.com>
2026-04-05 13:48:12 +0200

    Add launch script builder

---
+++ app/src/main/java/com/jfdedit3/winewe/launch/LaunchScriptBuilder.kt
@@ -0,0 +1,28 @@
+package com.jfdedit3.winewe.launch
+
+import com.jfdedit3.winewe.model.ContainerProfile
+import com.jfdedit3.winewe.model.ShortcutProfile
+
+object LaunchScriptBuilder {
+    fun build(container: ContainerProfile, shortcut: ShortcutProfile): String {
+        val envLines = buildMap {
+            putAll(container.box64Preset.env)
+            putAll(container.graphicsDriver.env)
+            container.envVars.forEach { put(it.key, it.value) }
+            if (shortcut.forceFullscreen) {
+                put("WINE_FULLSCREEN_FSR", "1")
+            }
+        }.entries.joinToString("\n") { "export ${it.key}=\"${it.value}\"" }
+
+        val args = shortcut.arguments.joinToString(" ") { "\"$it\"" }
+
+        return """
+#!/system/bin/sh
+export WINEPREFIX=\"/data/data/com.jfdedit3.winewe/files/runtime/containers/${container.id}/prefix\"
+export DISPLAY=\":0\"
+export PULSE_SERVER=\"127.0.0.1\"
+$envLines
+wine \"${shortcut.executablePath}\" $args
+""".trimIndent()
+    }
+}
